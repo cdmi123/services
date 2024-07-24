5488a37111a67640126b18f5be586f43ac57f8fc
cdmi123 <languagepdf@gmail.com>
2024-07-24 19:10:21 +0530

    msg

---
+++ footer.php
@@ -0,0 +1,205 @@
+﻿<footer class="footer">
+
+    <div class="footer-top aos" data-aos="fade-up">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-4 col-md-6">
+
+                    <div class="footer-widget">
+                        <div class="footer-logo">
+                            <a href="index.html"><img src="assets/img/logo.svg" alt="logo"></a>
+                        </div>
+                        <div class="footer-content">
+                            <p>Lorem ipsum dolor sit consectetur adipiscing elit, sed do eiusmod tempor commodo
+                                consequat. </p>
+                        </div>
+                        <div class="footer-selects">
+                            <h2 class="footer-title">Language & Currency</h2>
+                            <div class="row">
+                                <div class="col-lg-12 d-flex">
+                                    <div class="footer-select">
+                                        <img src="assets/img/icons/global.svg" alt="img">
+                                        <select class="select">
+                                            <option>English</option>
+                                            <option>France</option>
+                                            <option>Spanish</option>
+                                        </select>
+                                    </div>
+                                    <div class="footer-select">
+                                        <img src="assets/img/icons/dropdown.svg" class="footer-dropdown" alt="img">
+                                        <select class="select">
+                                            <option>US Dollars</option>
+                                            <option>INR</option>
+                                            <option>Kuwait</option>
+                                        </select>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+                <div class="col-lg-2 col-md-6">
+
+                    <div class="footer-widget footer-menu">
+                        <h2 class="footer-title">Quick Links</h2>
+                        <ul>
+                            <li>
+                                <a href="about-us.html">About Us</a>
+                            </li>
+                            <li>
+                                <a href="blog-grid.html">Blogs</a>
+                            </li>
+                            <li>
+                                <a href="contact-us.html">Contact Us</a>
+                            </li>
+                            <li>
+                                <a href="provider-signup.html">Become a Professional</a>
+                            </li>
+                            <li>
+                                <a href="user-signup.html">Become a User</a>
+                            </li>
+                        </ul>
+                    </div>
+
+                </div>
+                <div class="col-lg-3 col-md-6">
+
+                    <div class="footer-widget footer-contact">
+                        <h2 class="footer-title">Contact Us</h2>
+                        <div class="footer-contact-info">
+                            <div class="footer-address">
+                                <p><span><i class="feather-map-pin"></i></span> 367 Hillcrest Lane, Irvine, California,
+                                    United States</p>
+                            </div>
+                            <p><span><i class="feather-phone"></i></span> 321 546 8764</p>
+                        </div>
+                    </div>
+
+                </div>
+                <div class="col-lg-3 col-md-6">
+
+                    <div class="footer-widget">
+                        <h2 class="footer-title">Follow Us</h2>
+                        <div class="social-icon">
+                            <ul>
+                                <li>
+                                    <a href="javascript:void(0);"><i class="fa-brands fa-facebook"></i> </a>
+                                </li>
+                                <li>
+                                    <a href="javascript:void(0);"><i class="fab fa-twitter"></i> </a>
+                                </li>
+                                <li>
+                                    <a href="javascript:void(0);"><i class="fa-brands fa-instagram"></i></a>
+                                </li>
+                                <li>
+                                    <a href="javascript:void(0);"><i class="fa-brands fa-linkedin"></i></a>
+                                </li>
+                            </ul>
+                        </div>
+                        <h2 class="footer-subtitle">Newsletter Signup</h2>
+                        <div class="subscribe-form">
+                            <input type="email" class="form-control" placeholder="Enter Email Address">
+                            <button type="submit" class="btn footer-btn">
+                                <i class="feather-send"></i>
+                            </button>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <div class="footer-bottom">
+        <div class="container">
+
+            <div class="copyright">
+                <div class="row align-items-center">
+                    <div class="col-md-4">
+                        <div class="copyright-text">
+                            <p class="mb-0">Copyright &copy; 2023. All Rights Reserved.</p>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="payment-image">
+                            <ul>
+                                <li>
+                                    <a href="javascript:void(0);"><img src="assets/img/payment/visa.png" alt="img"></a>
+                                </li>
+                                <li>
+                                    <a href="javascript:void(0);"><img src="assets/img/payment/mastercard.png"
+                                            alt="img"></a>
+                                </li>
+                                <li>
+                                    <a href="javascript:void(0);"><img src="assets/img/payment/stripe.png"
+                                            alt="img"></a>
+                                </li>
+                                <li>
+                                    <a href="javascript:void(0);"><img src="assets/img/payment/discover.png"
+                                            alt="img"></a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+
+                        <div class="copyright-menu">
+                            <ul class="policy-menu">
+                                <li>
+                                    <a href="privacy-policy.html">Privacy Policy</a>
+                                </li>
+                                <li>
+                                    <a href="terms-condition.html">Terms & Conditions</a>
+                                </li>
+                            </ul>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+</footer>
+
+
+<div class="mouse-cursor cursor-outer"></div>
+<div class="mouse-cursor cursor-inner"></div>
+
+</div>
+
+<div class="progress-wrap active-progress">
+    <svg class="progress-circle svg-content" width="100%" height="100%" viewBox="-1 -1 102 102">
+        <path d="M50,1 a49,49 0 0,1 0,98 a49,49 0 0,1 0,-98"
+            style="transition: stroke-dashoffset 10ms linear 0s; stroke-dasharray: 307.919px, 307.919px; stroke-dashoffset: 228.265px;">
+        </path>
+    </svg>
+</div>
+
+
+<script data-cfasync="false" src="cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script>
+<script src="assets/js/jquery-3.7.0.min.js" type="bfe63e8502eda5625dd602fb-text/javascript"></script>
+
+<script src="assets/js/bootstrap.bundle.min.js" type="bfe63e8502eda5625dd602fb-text/javascript"></script>
+
+<script src="assets/js/feather.min.js" type="bfe63e8502eda5625dd602fb-text/javascript"></script>
+
+<script src="assets/js/owl.carousel.min.js" type="bfe63e8502eda5625dd602fb-text/javascript"></script>
+
+<script src="assets/plugins/select2/js/select2.min.js" type="bfe63e8502eda5625dd602fb-text/javascript"></script>
+
+<script src="assets/plugins/aos/aos.js" type="bfe63e8502eda5625dd602fb-text/javascript"></script>
+
+<script src="assets/js/backToTop.js" type="bfe63e8502eda5625dd602fb-text/javascript"></script>
+
+<script src="assets/js/script.js" type="bfe63e8502eda5625dd602fb-text/javascript"></script>
+<script src="cdn-cgi/scripts/7d0fa10a/cloudflare-static/rocket-loader.min.js"
+    data-cf-settings="bfe63e8502eda5625dd602fb-|49" defer></script>
+</body>
+
+<!-- Mirrored from truelysell.dreamstechnologies.com/html/template/index.html by HTTrack Website Copier/3.x [XR&CO'2014], Wed, 24 Jul 2024 12:54:39 GMT -->
+
+</html>
---
+++ header.php
@@ -0,0 +1,93 @@
+﻿<!DOCTYPE html>
+<html lang="en">
+
+    <!-- Mirrored from truelysell.dreamstechnologies.com/html/template/index.html by HTTrack Website Copier/3.x [XR&CO'2014], Wed, 24 Jul 2024 12:53:56 GMT -->
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport"
+            content="width=device-width, initial-scale=1.0, user-scalable=0">
+        <title>Truelysell | Template</title>
+
+        <link rel="shortcut icon" href="assets/img/favicon.png">
+
+        <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+
+        <link rel="stylesheet"
+            href="assets/plugins/fontawesome/css/fontawesome.min.css">
+        <link rel="stylesheet"
+            href="assets/plugins/fontawesome/css/all.min.css">
+
+        <link rel="stylesheet" href="assets/css/feather.css">
+
+        <link rel="stylesheet"
+            href="assets/plugins/select2/css/select2.min.css">
+
+        <link rel="stylesheet" href="assets/css/owl.carousel.min.css">
+
+        <link rel="stylesheet" href="assets/plugins/aos/aos.css">
+
+        <link rel="stylesheet" href="assets/css/style.css">
+    </head>
+    <body class="body-one">
+        <div class="main-wrapper">
+
+            <header class="header header-one">
+                <div class="container">
+                    <nav class="navbar navbar-expand-lg header-nav">
+                        <div class="navbar-header">
+                            <a id="mobile_btn" href="javascript:void(0);">
+                                <span class="bar-icon">
+                                    <span></span>
+                                    <span></span>
+                                    <span></span>
+                                </span>
+                            </a>
+                            <a href="index.php" class="navbar-brand logo">
+                                <img src="assets/img/logo.svg" class="img-fluid"
+                                    alt="Logo">
+                            </a>
+                            <a href="index.php"
+                                class="navbar-brand logo-small">
+                                <img src="assets/img/logo-small.png"
+                                    class="img-fluid" alt="Logo">
+                            </a>
+                        </div>
+                        <div class="main-menu-wrapper">
+                            <div class="menu-header">
+                                <a href="index.php" class="menu-logo">
+                                    <img src="assets/img/logo.svg"
+                                        class="img-fluid" alt="Logo">
+                                </a>
+                                <a id="menu_close" class="menu-close"
+                                    href="javascript:void(0);"> <i
+                                        class="fas fa-times"></i></a>
+                            </div>
+                            <ul class="main-nav">
+                                <li class="has-submenu megamenu active">
+                                    <a href="index.php">Home</a>
+                                </li>
+                                <li class="has-submenu">
+                                    <a href="services.php">Services</a>
+                                </li>
+                                <li class="login-link">
+                                    <a href="choose-signup.html">Register</a>
+                                </li>
+                                <li class="login-link">
+                                    <a href="login.html">Login</a>
+                                </li>
+                            </ul>
+                        </div>
+                        <ul class="nav header-navbar-rht">
+                            <li class="nav-item">
+                                <a class="nav-link header-reg"
+                                    href="choose-signup.html">Register</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link header-login"
+                                    href="login.html"><i
+                                        class="fa-regular fa-circle-user me-2"></i>Login</a>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+            </header>
